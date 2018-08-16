1f243c0e01257282ff5baeb9fd9c92666e5d5592
Yaremenko Miroslava Maksimovna <ext.myaremenko@tinkoff.ru>
2018-08-16 17:12:47 +0300

    Test bot

--- bot.js
+++ bot.js
@@ -1,29 +1,24 @@
-const Telegraf = require('telegraf');
-const express = require('express');
-const app = express();
-
 const API_TOKEN = '618490077:AAFQbPupzUUNHqADjHDlQu-RPAobpJLSYeM';
-const PORT = process.env.PORT || 3000;
-const URL = process.env.URL || 'https://schedule-telegram-bot.herokuapp.com/';
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(API_TOKEN, { polling: true });
 
-const bot = new Telegraf(API_TOKEN);
-bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
-app.use(bot.webhookCallback(`/bot${API_TOKEN}`));
+bot.on('message', (msg) => {
+  const { chat } = msg;
+  const chatId = chat.id;
+  const firstName = chat.first_name;
 
-
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'));
-
-
-bot.startPolling();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+  console.log(msg);
+  bot.sendMessage(chatId, `Hello, ${firstName}`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+//
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
