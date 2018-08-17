dccc6dd54b342c7055c87415cc6d0ba4fe219853
Yaremenko Miroslava Maksimovna <ext.myaremenko@tinkoff.ru>
2018-08-17 16:09:09 +0300

    Add basic comands

--- bot.js
+++ bot.js
@@ -1,24 +1,77 @@
-const API_TOKEN = '618490077:AAFQbPupzUUNHqADjHDlQu-RPAobpJLSYeM';
 const TelegramBot = require('node-telegram-bot-api');
+const MESSAGES = require('./messages');
+const API_TOKEN = '618490077:AAFQbPupzUUNHqADjHDlQu-RPAobpJLSYeM';
 const bot = new TelegramBot(API_TOKEN, { polling: true });
 
 bot.on('message', (msg) => {
-  const { chat } = msg;
-  const chatId = chat.id;
-  const firstName = chat.first_name;
+  const { chat: { id }, text } = msg;
 
-  console.log(msg);
-  bot.sendMessage(chatId, `Hello, ${firstName}`);
+  if (MESSAGES.GREETINGS.includes(text.toLocaleLowerCase())) {
+    bot.sendMessage(id, MESSAGES.GREETING_REPLY);
+  }
 });
 
-// const express = require('express');
-// const app = express();
-// const PORT = process.env.PORT || 3000;
+bot.onText(/\/startsearch/, (msg) => {
+  const { chat: { id }} = msg;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-//
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+  bot.sendMessage(id, MESSAGES.START_SEARCH_REPLY, {
+    reply_markup: {
+      keyboard: [['genre'], ['year'], ['name']],
+      one_time_keyboard: true
+    }
+  });
+});
+
+bot.onText(/\/endsearch/, (msg) => {
+  const { chat: { id }} = msg;
+
+  bot.sendMessage(id, MESSAGES.END_SEARCH_REPLY);
+});
+
+bot.onText(/genre/, (msg) => {
+  const { chat: { id }} = msg;
+
+  bot.sendMessage(id, MESSAGES.END_SEARCH_REPLY, {
+    reply_markup: {
+      remove_keyboard: true
+    }
+  });
+});
+
+bot.onText(/name/, (msg) => {
+  const { chat: { id }} = msg;
+
+  bot.sendMessage(id, MESSAGES.END_SEARCH_REPLY, {
+    reply_markup: {
+      remove_keyboard: true
+    }
+  });
+});
+
+bot.onText(/year/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, MESSAGES.END_SEARCH_REPLY, {
+    reply_markup: {
+      remove_keyboard: true
+    }
+  });
+});
+
+bot.onText(/\/top/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, MESSAGES.END_SEARCH_REPLY);
+});
+
+bot.onText(/\/next/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, MESSAGES.END_SEARCH_REPLY);
+});
+
+bot.on('polling_error', (error) => {
+  console.log(error);  // => 'EFATAL'
+});
+
+
---
+++ messages.js
@@ -0,0 +1,5 @@
+exports.START_SEARCH_REPLY = 'Select a criteria for choosing a movie, please :)\nname\ngenre\nyear';
+exports.END_SEARCH_REPLY = 'Thank you, my master. I hope, I help you. Enjoy a movie';
+exports.GREETINGS = ['hello', 'aloha', 'hi', 'good morning', 'good afternoon'];
+exports.GREETING_REPLY = 'Hello, my master. I can help you to find a great film.\nJust type /startsearch.';
+exports.HELP_REPLY = '';
